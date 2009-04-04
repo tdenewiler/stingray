88536a3e8863ba4a94c0093880945821e30f4765
Thomas Denewiler <tdenewiler@gmail.com>
2009-04-04 18:28:12 +0000

    I got the joystick working to send assisted teleoperation commands to nav. To use you have to have nav running, then start gui and select "Manual" as the operating mode, then start joydrive. Every time a button/axis is pushed it will change the corresponding target value by +/- 1. The buttons/axes are mapped to: 	- pitch: up/down on D-pad 	- roll: left/right on D-pad 	- fx: left/right on left joystick 	- fy: up/down on left joystick 	- yaw: left/right buttons on 4 buttons on right 	- depth: top/bottom buttons on 4 buttons on right 	- speed: top set of trigger buttons I am using a Playstation clone controller for development (actually a Logitech Cordless Rumblepad 2). This code will work for other types of joysticks but may need a different mapping for the buttons/axes.
    git-svn-id: file:///home/thomas/tmp/stingray/libs@15 ae4af862-681a-43e1-b28c-8c040b23e139

--- joy/test/joy_test.c
+++ joy/test/joy_test.c
@@ -7,43 +7,41 @@
 
 int main()
 {
-	int joy_fd = -1;
-	int status = -1;
-	JOY_DATA joy_data;
+    int joy_fd = -1;
+    int status = -1;
+    JOY_DATA joy_data;
 
-	/* Don't pass pointers of locals variables because of stack swaps. Instead
-	 * use the heap. */
-	JOY_DATA *pjoy_data = ( JOY_DATA * )calloc( 1, sizeof( JOY_DATA ) );
-	pjoy_data = &joy_data;
+    /* Don't pass pointers of locals variables because of stack swaps. Instead
+     * use the heap. */
+    JOY_DATA *pjoy_data = (JOY_DATA *)calloc( 1, sizeof(JOY_DATA) );
+    pjoy_data = &joy_data;
 
-	joy_fd = joy_setup( );
-	printf( "joy_fd = %d\n\n", joy_fd );
+    joy_fd = joy_setup( );
+    printf( "joy_fd = %d\n\n", joy_fd );
 
-	if ( joy_fd < 0 ) {
-		printf( "No joystick, quitting...\n" );
-		return 0;
-	}
+    if ( joy_fd < 0 ) {
+        printf( "No joystick, quitting...\n" );
+        return 0;
+    }
 
-	while ( 1 ) {
-		/* Fill in JOY_DATA struct. */
-		status = joy_get_data( joy_fd, pjoy_data );
+    while ( 1 ) {
+        /* Fill in JOY_DATA struct. */
+        status = joy_get_data( joy_fd, pjoy_data );
 
-		if ( status > 0 ) {
-			printf( "joy_axis = %d  axis_value = %d\n",
-			        joy_data.joy_axis,
-			        joy_data.axis_value
-			      );
-			printf( "joy_button = %d  button_value = %d\n",
-			        joy_data.joy_button,
-			        joy_data.button_value
-			      );
-		}
-	}
+        if ( status > 0 ) {
+            printf( "joy_axis = %d  axis_value = %d\n",
+                    joy_data.joy_axis,
+                    joy_data.axis_value
+                  );
+            printf( "joy_button = %d  button_value = %d\n",
+                    joy_data.joy_button,
+                    joy_data.button_value
+                  );
+        }
+    }
 
-	/* Free used heap memory and throw away the keys. */
+    /* Close the serial port. */
+    close( joy_fd );
 
-	/* Close the serial port. */
-	close( joy_fd );
-
-	return 0;
+    return 0;
 } /* end main() */
