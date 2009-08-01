2da2f53c3d5757be254ccd6ec99d4e6a120f5fe0
Thomas Denewiler <tdenewiler@gmail.com>
2009-08-01 13:42:50 +0000

    This is what Gideon was working on for vision. Mostly trying to find gate, fence and pipe thresholds.
    git-svn-id: file:///home/thomas/tmp/stingray/src@235 ae4af862-681a-43e1-b28c-8c040b23e139

--- visiond/visiond.c
+++ visiond/visiond.c
@@ -307,7 +307,7 @@ int main( int argc, char *argv[] )
 				printf("MAIN: Loading dir %s\n", filename);
 				//strncat( filename, dfile->d_name, STRING_SIZE );
 				//strncat( filename, "20090730_135401.54264.jpg", STRING_SIZE );
-				strncat( filename, "20090730_135359.819759.jpg", STRING_SIZE );
+				strncat( filename, "20090730_135402.489205.jpg", STRING_SIZE );
 
 				printf("MAIN: Loading file %s\n", filename);
 				img = cvLoadImage( filename );
@@ -406,7 +406,7 @@ int main( int argc, char *argv[] )
 			/* Load image here. */
 			strncpy( filename, dirname, STRING_SIZE );
 			//strncat( filename, dfile->d_name, STRING_SIZE );
-			strncat( filename, "20090730_135359.819759.jpg", STRING_SIZE );
+			strncat( filename, "20090730_135402.489205.jpg", STRING_SIZE );
 
 			img = cvLoadImage( filename );
 		}
